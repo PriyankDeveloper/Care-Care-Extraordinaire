99219a716c03027c289c2a36ab51df7cbda6c5c2
Priyank <priyankpatel386@hotmail.com>
2017-09-13 22:52:40 -0400

    Manage Vehicle Page

---
+++ CarCre/Controllers/VehicleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarCare.Controllers
+{
+    public class VehicleController : Controller
+    {
+        // GET: Vehicle
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
