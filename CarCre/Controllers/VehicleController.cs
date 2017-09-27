afc6986aceabfeb67b91862885e6353ee23f98d6
Priyank <priyankpatel386@hotmail.com>
2017-09-26 22:12:28 -0400

    JQGrid for vehicle

--- CarCre/Controllers/VehicleController.cs
+++ CarCre/Controllers/VehicleController.cs
@@ -21,5 +21,89 @@ namespace CarCare.Controllers
             return Json(true,JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult GetTodoLists(string sidx, string sord, int page, int rows)  //Gets the todo Lists.
+        {
+            int pageIndex = Convert.ToInt32(page) - 1;
+            int pageSize = rows;
+
+            Vehicle vh = new Vehicle()
+            {
+                OwnerId = 1,
+                VechicleDealer = "Test",
+                VechicleYear = "2017",
+                VehicleId = 12,
+                VehicleMark = "Honda",
+                VehicleModel = "Accord",
+                VINNumber = "ASDF-WER-ERWER"
+            };
+
+            int totalRecords = 1;
+            var totalPages = (int)Math.Ceiling((float)totalRecords / (float)rows);
+
+            var jsonData = new
+            {
+                total = 1,
+                page = 1,
+                records = totalRecords,
+                rows = vh
+            };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
+
+        // TODO:insert a new row to the grid logic here
+        [HttpPost]
+        public string Create([Bind(Exclude = "Id")] Vehicle objTodo)
+        {
+            string msg;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //db.TodoLists.Add(objTodo);
+                    //db.SaveChanges();
+                    msg = "Saved Successfully";
+                }
+                else
+                {
+                    msg = "Validation data not successfull";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "Error occured:" + ex.Message;
+            }
+            return msg;
+        }
+        public string Edit(Vehicle objTodo)
+        {
+            string msg;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //db.Entry(objTodo).State = EntityState.Modified;
+                    //db.SaveChanges();
+                    msg = "Saved Successfully";
+                }
+                else
+                {
+                    msg = "Validation data not successfull";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "Error occured:" + ex.Message;
+            }
+            return msg;
+        }
+
+        public string Delete(int Id)
+        {
+            //TodoList todolist = db.TodoLists.Find(Id);
+            //db.TodoLists.Remove(todolist);
+            //db.SaveChanges();
+            return "Deleted successfully";
+        }
+
     }
 }
--- CarCre/Scripts/vehicleManagement.js
+++ CarCre/Scripts/vehicleManagement.js
@@ -1,1 +1,72 @@
-﻿
+﻿$(function () {
+    $("#grid").jqGrid({
+        url: "/Vehicle/GetTodoLists",
+        datatype: 'json',
+        mtype: 'Get',
+        colNames: ['OwnerId', 'VechicleDealer', 'VechicleYear', 'VehicleId', 'VehicleMark', 'VehicleModel', 'VINNumber'],
+        colModel: [
+            { hidden: true, name: 'OwnerId', index: 'OwnerId', editable: true },
+            { name: 'VechicleDealer', index: 'VechicleDealer', width: 150, stype: 'text', sortable: true, editable: true },
+            { name: 'VechicleYear', index: 'VechicleYear', width: 150, stype: 'text',editable: true },
+            { name: 'VehicleId', index: 'VehicleId', width: 60, editable: true },
+            { name: 'VehicleMark', index: 'VehicleMark', width: 80, stype: 'text', align: "right", editable: true },
+            { name: 'VehicleModel', index: 'VehicleModel', width: 40, stype: 'text', align: "right", editable: true },
+            { name: 'VINNumber', index: 'VINNumber', width: 150, stype: 'text', sortable: false, editable: true }],
+        pager: jQuery('#pager'),
+        rowNum: 10,
+        rowList: [10, 20, 30, 40],
+        height: '100%',
+        viewrecords: true,
+        caption: 'Todo List',
+        emptyrecords: 'No records to display',
+        jsonReader: {
+            root: "rows",
+            page: "page",
+            total: "total",
+            records: "records",
+            repeatitems: false,
+            Id: "1"
+        },
+        autowidth: true,
+        multiselect: false
+    }).navGrid('#pager', { edit: true, add: true, del: true, search: false, refresh: true },
+        {
+            // edit options
+            zIndex: 100,
+            url: '/Vehicle/Edit',
+            closeOnEscape: true,
+            closeAfterEdit: true,
+            recreateForm: true,
+            afterComplete: function (response) {
+                if (response.responseText) {
+                    alert(response.responseText);
+                }
+            }
+        },
+        {
+            // add options
+            zIndex: 100,
+            url: "/Vehicle/Create",
+            closeOnEscape: true,
+            closeAfterAdd: true,
+            afterComplete: function (response) {
+                if (response.responseText) {
+                    alert(response.responseText);
+                }
+            }
+        },
+        {
+            // delete options
+            zIndex: 100,
+            url: "/Vehicle/Delete",
+            closeOnEscape: true,
+            closeAfterDelete: true,
+            recreateForm: true,
+            msg: "Are you sure you want to delete this task?",
+            afterComplete: function (response) {
+                if (response.responseText) {
+                    alert(response.responseText);
+                }
+            }
+        });
+});
