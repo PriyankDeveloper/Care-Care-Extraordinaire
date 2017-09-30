26324cd0a30524219db76469cca474f921eda1e2
Priyank <priyankpatel386@hotmail.com>
2017-09-30 15:25:43 -0400

    ViewModels

---
+++ CarCre/Models/ServiceRecordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarCare.Models
+{
+    public class ServiceRecordViewModel
+    {
+        public long ServiceId { get; set; }
+        public long VehicleId { get; set; }
+        public long ServiceTypeId { get; set; }
+        public Nullable<System.DateTime> ServiceDate { get; set; }
+        public Nullable<long> ServiceStationId { get; set; }
+        public Nullable<System.DateTime> CompletedDate { get; set; }
+        public Nullable<System.DateTime> LastModifiedDate { get; set; }
+        public Nullable<decimal> ServiceCost { get; set; }
+    }
+}
---
+++ CarCre/Models/ServiceStationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarCare.Models
+{
+    public class ServiceStationViewModel
+    {
+        public long ServiceStationId { get; set; }
+        public string OwnedBy { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public Nullable<int> ZipCode { get; set; }
+        public string StreetAddress { get; set; }
+    }
+}
