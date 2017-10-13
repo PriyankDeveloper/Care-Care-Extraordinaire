7a3a52cb7d89db86422630cef2f5dfcf416c423e
Priyank <priyankpatel386@hotmail.com>
2017-10-13 13:33:31 -0400

    login page and home page

--- CarCre/Controllers/HomeController.cs
+++ CarCre/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CarCare.BusinessLogic;
+using CarCare.Models;
+using DotNet.Highcharts;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +13,43 @@ namespace CarCare.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private IBusinessInterface BusinessInterface;
+
+        public HomeController(IBusinessInterface businessInterface)
         {
-            return View();
+            BusinessInterface = businessInterface;
         }
 
+        public ActionResult Index()
+        {
+            Charts ct = new Charts();
+
+            var allVehicles = BusinessInterface.GetAllVehicles().ToList();         
+
+            Highcharts chart = new Highcharts("Chart")
+                .SetXAxis(new XAxis
+                {
+                    Categories = allVehicles.Select(i => i.VINNumber).ToArray()
+
+                })
+                .SetSeries(new Series
+                {
+                    Data = new Data(allVehicles.Select(i=>i.VechicleYear).Distinct().ToArray())
+                });
+            ct.highCharts = chart;
+            return View(ct);
+        }
+
+        [HttpGet]
+        public JsonResult GetChartData()
+        {
+
+            var a = BusinessInterface.GetAllVehicles().GroupBy(item => item.VechicleYear)
+        .Select(group => new { Customer = group.Key, Items = group.ToList() })
+        .ToList();
+
+        return Json(a, JsonRequestBehavior.AllowGet);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ CarCre/Models/Charts.cs
@@ -0,0 +1,13 @@
+﻿using DotNet.Highcharts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarCare.Models
+{
+    public class Charts
+    {
+        public Highcharts highCharts { get; set; }
+    }
+}
