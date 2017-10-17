da3550e1e2d43b57e30a4d21802aa970984571f0
Thomas <hildesht@hotmail.com>
2017-10-16 20:19:49 -0600

    Sumerization record entry completed

---
+++ CarCre/Controllers/SumerizationController.cs
@@ -0,0 +1,36 @@
+﻿using CarCare.BusinessLogic;
+using CarCare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarCare.Controllers
+{
+    public class SumerizationController : BaseServiceRecordController
+    {
+        public SumerizationController(IBusinessInterface businessInterface)
+            :base(businessInterface, 8)
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return base.BaseIndex();
+        }
+
+        public ActionResult Add()
+        {
+            var serviceRecordViewModel = new ServiceRecordViewModel();
+            serviceRecordViewModel.ControllerName = "Sumerization";
+            return base.AddNewRecord(serviceRecordViewModel);
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return base.EditRecord(id);
+        }
+    }
+}
