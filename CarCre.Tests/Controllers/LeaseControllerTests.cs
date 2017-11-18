ce6e0df0a641ba30ea6945617f33fde03d1aee2e
SY <shaoyan33@gmail.com>
2017-11-18 01:54:15 -0800

    Added unit tests for repair, insurance, lease and warranty

---
+++ CarCre.Tests/Controllers/LeaseControllerTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarCare.Controllers;
+using CarCare.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCre.Tests.Controllers
+{
+    [TestClass]
+    public class LeaseControllerTests
+    {
+        [TestMethod()]
+        public void AddNewLeaseRecordTest()
+        {
+            // Initialize
+            CarCareBusinessLogic useTest = new CarCareBusinessLogic();
+
+            var newVehicle = useTest.SaveVehicle(new CarCare.CarCareDatabase.Vehicle()
+            {
+                VehicleMark = "Toyata",
+                VehicleModel = "Camery",
+                VechicleYear = "1997",
+                VechicleDealer = "ABC",
+                OwnerId = 2,
+                VINNumber = "111-22-3333"
+            });
+            
+            // create new lease record
+            var newLeaseRecord = useTest.SaveLeaseRecord(new CarCare.CarCareDatabase.LeaseRecord()
+            {
+                LeaseStartDate = DateTime.Now,
+                LeaseTerm = 36,
+                VehicleId = newVehicle.VehicleId,
+                Company = "Acompany",
+                MonthlyPayment = 250,
+                LeaseNotes = "unit test"
+            });
+
+            // Assert
+            Assert.IsTrue(newLeaseRecord.MonthlyPayment == 250);
+            Assert.AreEqual(newLeaseRecord.VehicleId, newVehicle.VehicleId);
+            Assert.AreEqual(newLeaseRecord.LeaseTerm, 36);
+            Assert.AreEqual(newLeaseRecord.Company, "Acompany");
+            Assert.AreEqual(newLeaseRecord.LeaseNotes, "unit test");
+
+            // delete created records
+            useTest.DeleteLeaseRecord(newLeaseRecord.LeaseId);
+            useTest.DeleteVehicle((int)newVehicle.VehicleId);
+        }
+
+        [TestMethod()]
+        public void EditLeaseRecordTest()
+        {
+            // Initialize
+            CarCareBusinessLogic useTest = new CarCareBusinessLogic();
+            CarCare.CarCareDatabase.LeaseRecord leaseRecord = useTest.GetAllLeaseRecords(1)[0];
+            CarCare.CarCareDatabase.LeaseRecord updatedLeaseRecord = leaseRecord;
+
+
+            // update insurance record
+            updatedLeaseRecord.LeaseTerm = 99;
+            updatedLeaseRecord.MonthlyPayment = 2000;
+            updatedLeaseRecord.Company = "Bcompany";
+            updatedLeaseRecord.LeaseNotes = "Edit unit test";
+
+            var curLeaseRecord = useTest.SaveLeaseRecord(updatedLeaseRecord);
+
+            // Assert
+            Assert.IsTrue(curLeaseRecord.LeaseTerm == 99);
+            Assert.AreEqual(curLeaseRecord.MonthlyPayment, 2000);
+            Assert.AreEqual(curLeaseRecord.Company, "Bcompany");
+            Assert.AreEqual(curLeaseRecord.LeaseNotes, "Edit unit test");
+
+            // reset values
+            useTest.SaveLeaseRecord(leaseRecord);
+        }
+
+    }
+}
---
+++ CarCre.Tests/Controllers/WarrantyControllerTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarCare.Controllers;
+using CarCare.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCre.Tests.Controllers
+{
+    [TestClass]
+    public class WarrantyControllerTests
+    {
+        [TestMethod()]
+        public void AddNewWarrantyRecordTest()
+        {
+            // Initialize
+            CarCareBusinessLogic useTest = new CarCareBusinessLogic();
+
+            var newVehicle = useTest.SaveVehicle(new CarCare.CarCareDatabase.Vehicle()
+            {
+                VehicleMark = "Toyata",
+                VehicleModel = "Camery",
+                VechicleYear = "1997",
+                VechicleDealer = "ABC",
+                OwnerId = 2,
+                VINNumber = "111-22-3333"
+            });
+
+            // create new waranty record
+            var newWarrantyRecord = useTest.SaveWarrantyRecord(new CarCare.CarCareDatabase.Warranty
+            {
+                PolicyNumber = "N100-20-3000",
+                WarrantyStartDate = DateTime.Now,
+                WarrantyExpirationDate = DateTime.Now,
+                WarrantyCost = 750,
+                VehicleId = newVehicle.VehicleId,
+                WarrantyProvider = "Acompany",
+                WarrantyCoverage = "default coverage"
+
+            });
+
+            // Assert
+            Assert.IsTrue(newWarrantyRecord.PolicyNumber == "N100-20-3000");
+            Assert.AreEqual(newWarrantyRecord.WarrantyCost, 750);
+            Assert.AreEqual(newWarrantyRecord.VehicleId, newVehicle.VehicleId);
+            Assert.AreEqual(newWarrantyRecord.WarrantyProvider, "Acompany");
+            Assert.AreEqual(newWarrantyRecord.WarrantyCoverage, "default coverage");
+
+            // delete created records
+            useTest.DeleteWarrantyRecord(newWarrantyRecord.WarrantyId);
+            useTest.DeleteVehicle((int)newVehicle.VehicleId);
+        }
+
+        [TestMethod()]
+        public void EditWarrantyRecordTest()
+        {
+            // Initialize
+            CarCareBusinessLogic useTest = new CarCareBusinessLogic();
+            CarCare.CarCareDatabase.Warranty warrantyRecord = useTest.GetAllWarrantyRecords(1)[0];
+            CarCare.CarCareDatabase.Warranty updatedWarrantyRecord = warrantyRecord;
+
+
+            // update insurance record
+            updatedWarrantyRecord.PolicyNumber = "new_N100-20-3000";
+            updatedWarrantyRecord.WarrantyCost = 2000;
+            updatedWarrantyRecord.WarrantyProvider = "Bcompany";
+            updatedWarrantyRecord.WarrantyCoverage = "Edit unit test";
+
+            var curWarrantyRecord = useTest.SaveWarrantyRecord(updatedWarrantyRecord);
+
+            // Assert
+            Assert.IsTrue(curWarrantyRecord.PolicyNumber == "new_N100-20-3000");
+            Assert.AreEqual(curWarrantyRecord.WarrantyCost, 2000);
+            Assert.AreEqual(curWarrantyRecord.WarrantyProvider, "Bcompany");
+            Assert.AreEqual(curWarrantyRecord.WarrantyCoverage, "Edit unit test");
+
+            // reset values
+            useTest.SaveWarrantyRecord(warrantyRecord);
+        }
+    }
+}
